ab463f2e775667e83515efeae41fc482b3fdfeda
Kaan <34076340+kaanalgan@users.noreply.github.com>
2020-05-18 01:44:09 +0300

    Implemented signInWithEmail method

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,36 +1,20 @@
 function getRegisterPage() {
-    location.href = "register.html";
+  location.href = "register.html";
 }
-function signIn(){
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var password_again = document.getElementById('password-again').value;
-    if(checkPass(password,password_again)){
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // ...
-          });
-    }
-    getLoginPage();
-}
-function signInWithFacebook() {
-    var provider = new firebase.auth.FacebookAuthProvider();
-    provider.addScope('email');
-    firebase.auth().signInWithRedirect(provider).then(function (result) {
-      var token = result.credential.accessToken;
-      var user = result.user;
-      console.log(user);
 
-    }).catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      console.log(errorCode,errorMessage);
-    });
-  }
+function signInWithEmail() {
+  var email = document.getElementById('inputEmail').value;
+  console.log('Email: ' + email);
+  var password = document.getElementById('inputPassword').value;
+  console.log('Password: ' + password);
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    console.log("No such account!");
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    location.href = "register.html";
+  });
+  console.log("Successful authentication");
+  location.href = "/users/applicant/applicant-dashboard";
+}
