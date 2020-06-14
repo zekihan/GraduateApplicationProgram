ed973c5fa8b2ea9a03575b1243172b5f1806ffcd
Kaplan80 <kaplan6398@gmail.com>
2020-06-14 21:14:45 +0300

    Program management eklendi sekme olarak
    program management sekme kısmına eklenmemiş, set interview yaparken.

---
+++ public/javascript/manage-programs.js
@@ -0,0 +1,149 @@
+
+
+
+
+
+function getAddProgramPage() {
+    location.href = "set-program.html";
+    
+}
+
+
+
+function getPrograms() {
+    
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            var programArray=new Array();
+            var userId = firebase.auth().currentUser.uid;
+            var userRef =firebase.database().ref("users/" + userId);
+            userRef.once("value").then(function (user) {
+                var departmentId = user.child("department").val().toString();
+                console.log("deptId: " + departmentId);
+                var phDRef =  firebase.database().ref('departments/' + departmentId + '/program/phd');
+                var masterRef =  firebase.database().ref('departments/' + departmentId + '/program/masters');
+                masterRef.once("value").then(function (masterPrograms) {
+                      masterPrograms.forEach(function (program) {
+                        console.log("name masters: "+ program.child("name").val().toString());
+                        programArray.push({
+                        type:"MASTER",
+                        id:program.key,
+                        name: program.child("name").val(),
+                        isTaking: program.child("isTaking").val(),
+                        explanation:program.child("explanation").val()                          
+                      });
+                });
+                phDRef.once("value").then(function (phdPrograms) {
+                    phdPrograms.forEach(function (program) {
+                        console.log("name phd: "+ program.child("name").val().toString());
+                        programArray.push({
+                        type:"PHD",
+                        id:program.key,
+                        name: program.child("name").val(),
+                        isTaking: program.child("isTaking").val(),
+                        explanation:program.child("explanation").val()                       
+                    });
+              });
+              displayPrograms(programArray,departmentId);
+                });
+            });
+        });
+    }
+    });
+}
+
+
+
+function removeProgram(programId,deptId,programType) {
+    var rootRef = firebase.database().ref();
+    if (programType == "MASTER") {
+        var masterRef=rootRef.child('departments/' + deptId + '/program/masters');
+        masterRef.child(programId).remove();
+    } else  if (programType == "PHD") {
+        var masterRef=rootRef.child('departments/' + deptId + '/program/phd');
+        masterRef.child(programId).remove();
+    }
+
+}
+
+
+
+function displayPrograms(programs,deptId) {
+    programs.forEach(function (program) {
+        //Create the outermost container
+        var programRow = document.createElement("DIV");
+        programRow.classList.add("media");
+        programRow.classList.add("text-muted");
+        programRow.classList.add("pt-3");
+        programRow.classList.add("border-bottom");
+        programRow.classList.add("border-gray");
+        programRow.style.cursor = "pointer";
+
+        //Set up the logo img
+        var img = document.createElement("IMG");
+        img.src = "../../assets/iyte-logo.svg";
+        img.style.width = "40px";
+        img.style.height = "40px";
+        img.style.marginBottom = "1rem";
+        img.style.background = "#ffffff";
+        img.style.color = "#ffffff";
+        img.classList.add("mr-2");
+        img.classList.add("rounded");
+        programRow.appendChild(img);
+
+        //Container for program name and type
+        var programTypeAndExplanation = document.createElement("P");
+        programTypeAndExplanation.classList.add("media-body");
+        programTypeAndExplanation.classList.add("pb-3");
+        programTypeAndExplanation.classList.add("mb-0");
+        programTypeAndExplanation.classList.add("small");
+        programTypeAndExplanation.classList.add("lh-125");
+
+        var programTitle = document.createElement("STRONG");
+        programTitle.classList.add("d-block");
+        programTitle.classList.add("text-gray-dark");
+        programTitle.innerHTML = program.type + "-" + program.name;
+        programTypeAndExplanation.appendChild(programTitle);
+
+       
+        var programExplanation = document.createElement("P");
+        var programText =program.explanation;
+      
+
+        programExplanation.innerHTML = programText;
+        programTypeAndExplanation.appendChild(programExplanation);
+       
+        programRow.appendChild(programTypeAndExplanation);
+
+        
+        var programLink = document.createElement("A");
+        programLink.classList.add("details-link");
+        programLink.innerHTML = "Remove Program";
+        programLink.style.color = "#c81912";      
+        programRow.onclick = function () {
+            if (confirm("The " + program.name + " program will be deleted. Are you sure?")) {
+                removeProgram(program.id,deptId,program.type);
+              alert("Program is deleted!");
+              location.href = "manage-programs.html";
+            }                     
+        
+        }
+        programRow.appendChild(programLink);
+        
+        programRow.style.marginTop = "1rem";
+        document.getElementById("list-container").appendChild(programRow);
+    });
+
+    var element = document.getElementById("spinner");
+        element.parentNode.removeChild(element);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ public/javascript/set-program.js
@@ -0,0 +1,100 @@
+
+
+function setProgram(){
+    
+    //console.log('setProgram içinde');
+    /*
+    //Decode the URL and get the parameters
+    var queryString = decodeURIComponent(window.location.search);
+    queryString = queryString.substring(1);
+    var queries = queryString.split("&");
+
+    //Get the application id from the url
+    var applicationId = (queries[0].split("="))[1];
+    var termInfo = (queries[1].split("="))[1];
+    var department = (queries[2].split("="))[1];
+    */
+
+   var e = document.getElementById("type-field");
+   var typeIn = e.options[e.selectedIndex].text;
+   var nameIn = $('#name-field').val();
+   var explanationIn = $('#explanation-field').val();
+
+
+    if (!areInputsValid(e,typeIn,nameIn,explanationIn)) {
+        return;
+    }
+
+    
+    
+
+    var userId = firebase.auth().currentUser.uid;
+    var userRef =firebase.database().ref("users/" + userId);
+    
+    userRef.once("value").then(function (user) {
+        var departmentId = user.child("department").val().toString();
+        var rootRef = firebase.database().ref();
+        var masterRef = rootRef.child('departments/' + departmentId + '/program/masters');
+        var newMasterRef = masterRef.push();
+        var phdRef = rootRef.child('departments/' + departmentId + '/program/phd');
+        var newPhdRef = phdRef.push();
+        console.log("typeInput: " + typeIn);
+        console.log("name: " + nameIn);
+    
+        if(typeIn == "Master") {
+            console.log("Master içinde");
+            newMasterRef.set ({
+                isTaking: 1,
+                name : nameIn,
+                explanation : explanationIn
+            }); 
+            
+        }
+        else if(typeIn == "Phd") {
+            newPhdRef.set ({
+                isTaking: 1,
+                name : nameIn,
+                explanation : explanationIn
+            });
+            
+        }
+        
+        location.href = "manage-programs.html";  
+    
+    });
+
+}
+
+
+
+function areInputsValid(e,type,name,explanation) {
+    $('.error-msg').text("");
+    var isValid=true;
+    console.log(type);
+    if (name == "") {
+        $("#name-error").text("Please enter a name.");
+        isValid=false;
+    } 
+           
+    if (explanation == "" ) {
+        $("#explanation-error").text("Please enter a explanation.");
+        isValid=false;
+    }  
+    
+    if (type == "Choose...") {
+        $("#type-error").text("Please choose a type.");
+        isValid=false;
+    } 
+    return isValid;
+   
+}
+
+
+/*
+function getApplicationInfoWithId(applicationId, term, department) {
+    console.log("application id is " + applicationId);
+    var id = applicationId;
+    var queryString = "?id=" + id + '&term=' + term + '&department=' + department;
+    window.location.href = "student-review.html" + queryString;
+}
+*/
