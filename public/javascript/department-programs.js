5c03f88a15de7bde37b25c196bccab3d39c60755
baros-code <60608553+baros-code@users.noreply.github.com>
2020-06-14 21:52:26 +0300

    Applicant'a available programs gösteriliyor

---
+++ public/javascript/department-programs.js
@@ -0,0 +1,182 @@
+
+
+
+
+
+
+
+
+function getDepartmentPrograms() {
+    
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+
+
+            var departmentId = parseDepartmentId();
+
+            firebase.database().ref("departments/" + departmentId).once('value').then(function (snapshot) {
+                var department = snapshot.child("name").val();
+                displayTitle(department);
+            
+            }).catch(function (error) {
+                console.log(error);
+            });
+
+
+            var programArray=new Array();
+            var userId = firebase.auth().currentUser.uid;
+            var userRef =firebase.database().ref("users/" + userId);
+            userRef.once("value").then(function (user) {
+                var phDRef =  firebase.database().ref('departments/' + departmentId + '/program/phd');
+                var masterRef =  firebase.database().ref('departments/' + departmentId + '/program/masters');
+                masterRef.once("value").then(function (masterPrograms) {
+                      masterPrograms.forEach(function (program) {
+                        console.log("name masters: "+ program.child("name").val().toString());
+                        programArray.push({
+                        type:"MASTER",
+                        id:program.key,
+                        name: program.child("name").val(),
+                        isTaking: program.child("isTaking").val(),
+                        explanation:program.child("explanation").val()                          
+                      });
+                });
+                phDRef.once("value").then(function (phdPrograms) {
+                    phdPrograms.forEach(function (program) {
+                        console.log("name phd: "+ program.child("name").val().toString());
+                        programArray.push({
+                        type:"PHD",
+                        id:program.key,
+                        name: program.child("name").val(),
+                        isTaking: program.child("isTaking").val(),
+                        explanation:program.child("explanation").val()                       
+                    });
+              });
+              displayPrograms(programArray);
+                });
+            });
+        });
+    }
+    });
+}
+
+
+function parseDepartmentId() {
+    var queryString = decodeURIComponent(window.location.search);
+    queryString = queryString.substring(1);
+    var queries = queryString.split("=");
+    return queries[1];
+}
+
+function displayTitle(department){
+    document.getElementById("department-title").innerHTML = department + " Department Available Programs";
+}
+
+
+
+function displayPrograms(programs) {
+    console.log(programs.toString());
+    var ol = document.createElement("OL");
+    ol.id = "sortable";
+    ol.style.listStyleType = "none";
+
+    //Common term and department information
+   // var term = applicants[0].term;
+    //var department = applicants[0].department;
+
+    programs.forEach(function (program) {
+
+        //Container list item element
+        var li = document.createElement("LI");
+        li.classList.add("ui-state-default");
+        li.id = program.id; // birden fazla id sıkıntı yaratır mı?
+
+        //Application data row.
+        var outerDiv = document.createElement('DIV');
+        outerDiv.classList.add("text-muted");
+        outerDiv.classList.add("pt-3");
+        outerDiv.classList.add("border-bottom");
+        outerDiv.classList.add("border-gray");
+        outerDiv.classList.add("media");
+        outerDiv.classList.add("applicant-info");
+        outerDiv.style.cursor = "pointer";
+
+        //IZTECH logo for each application.
+        var img = document.createElement("IMG");
+        img.src = "../../assets/iyte-logo.svg";
+        img.style.width = "40px";
+        img.style.height = "40px";
+        img.style.marginBottom = "1rem";
+        img.style.background = "#ffffff";
+        img.style.color = "#ffffff";
+        img.classList.add("mr-2");
+        img.classList.add("rounded");
+
+        outerDiv.appendChild(img);
+
+
+        var innerDiv = document.createElement("DIV");
+        innerDiv.classList.add("media-body");
+        innerDiv.classList.add("pb-3");
+        innerDiv.classList.add("mb-0");
+        innerDiv.classList.add("small");
+        innerDiv.classList.add("lh-125");
+
+
+        /* Program's name and type. */
+        var innerMostDiv = document.createElement("DIV");
+        innerMostDiv.classList.add("d-flex");
+        innerMostDiv.classList.add("justify-content-between");
+        innerMostDiv.classList.add("align-items-center");
+        innerMostDiv.classList.add("w-100");
+
+        var strong = document.createElement("STRONG");
+        strong.classList.add("text-gray-dark");
+        strong.innerHTML = program.type + " - " + program.name;
+        innerMostDiv.appendChild(strong);
+
+        innerDiv.appendChild(innerMostDiv);
+
+        
+        var span = document.createElement("SPAN");
+        span.classList.add("d-block");
+        if(program.explanation)
+            span.innerHTML = program.explanation;
+        else{
+            span.innerHTML = "No explanation provided."
+        }
+        innerDiv.appendChild(span);
+
+        outerDiv.appendChild(innerDiv);
+
+        li.appendChild(outerDiv);
+        
+        
+
+        /* Acceptance checkbox 
+        var containerLabel = document.createElement("LABEL");
+        var checkbox = document.createElement("INPUT");
+        checkbox.id = "indeterminate-checkbox";
+        checkbox.type = "checkbox";
+        checkbox.style = "margin-right: 4px;"
+        containerLabel.appendChild(checkbox);
+        */
+        
+        /*
+       var containerLabel = document.createElement("LABEL");
+       var innerSpan = document.createElement("SPAN");
+       innerSpan.classList.add("checkbox-label");
+       innerSpan.style.color="#c81912";
+       innerSpan.style.fontSize="13px";
+       innerSpan.innerHTML = "Remove Program";
+       containerLabel.appendChild(innerSpan);
+        */
+       var containerLabel = document.createElement("LABEL");
+
+       outerDiv.appendChild(containerLabel);
+
+        ol.appendChild(li);
+    });
+
+    document.getElementById("list-container").removeChild(document.getElementById("spinner"));
+    document.getElementById("list-container").appendChild(ol);
+}
---
+++ public/javascript/list-departments-1.js
@@ -0,0 +1,131 @@
+
+
+
+
+
+function getDepartments() {
+    firebase.database().ref('departments').once('value').then(function (departments) {
+        var allDepartments = new Array();
+        var departmentsPrograms = new Array();
+        departments.forEach(function (department) {
+            var departmentsPrograms = new Array();
+            department.child("program").forEach(function (program) {
+                var degree = program.key;
+                program.forEach(function (programId) {
+                    //If the program is accepting students
+                    if (programId.child("isTaking").val() === 1) {
+                        departmentsPrograms.push({
+                            degree: prettyFormat(program.key),
+                            id: programId.key,
+                            name: programId.child("name").val()
+                        });
+                    }
+                });
+            });
+            allDepartments.push({
+                departmentName: department.child("name").val(),
+                isConfirmed: department.child("confirmed").val(),
+                departmentId: department.key,
+                programs: departmentsPrograms
+            });
+        });
+        displayDepartments(allDepartments);
+    });
+}
+
+
+
+function prettyFormat(output) {
+    switch (output) {
+        case "masters":
+            return "M.Sc";
+        case "phd":
+            return "PhD";
+        default:
+            break;
+    }
+}
+
+
+function displayDepartments(departments) {
+    departments.forEach(function (department) {
+        //Department Name for the header
+        var deptHeader = document.createElement("LABEL");
+        deptHeader.innerHTML = department.departmentName;
+
+        //Create the outermost container
+        var departmentRow = document.createElement("DIV");
+        departmentRow.classList.add("media");
+        departmentRow.classList.add("text-muted");
+        departmentRow.classList.add("pt-3");
+        departmentRow.classList.add("border-bottom");
+        departmentRow.classList.add("border-gray");
+        departmentRow.style.cursor = "pointer";
+
+        //Set up the logo img
+        var img = document.createElement("IMG");
+        img.src = "../../assets/iyte-logo.svg";
+        img.style.width = "40px";
+        img.style.height = "40px";
+        img.style.marginBottom = "1rem";
+        img.style.background = "#ffffff";
+        img.style.color = "#ffffff";
+        img.classList.add("mr-2");
+        img.classList.add("rounded");
+        departmentRow.appendChild(img);
+
+        //Container for department name and programs below it
+        var departmentAndProgramText = document.createElement("P");
+        departmentAndProgramText.classList.add("media-body");
+        departmentAndProgramText.classList.add("pb-3");
+        departmentAndProgramText.classList.add("mb-0");
+        departmentAndProgramText.classList.add("small");
+        departmentAndProgramText.classList.add("lh-125");
+
+        var departmentName = document.createElement("STRONG");
+        departmentName.classList.add("d-block");
+        departmentName.classList.add("text-gray-dark");
+        departmentName.innerHTML = department.departmentName;
+        departmentAndProgramText.appendChild(departmentName);
+
+        var programName = document.createElement("P");
+        var programText = "Programs :";
+
+        //Get programs(degrees)
+        if(department.programs[0] != null){
+            programText += " " + department.programs[0].degree;
+            if(department.programs[1] != null){
+                programText += " & " + department.programs[1].degree;
+            }
+        }
+
+        programName.innerHTML = programText;
+        departmentAndProgramText.appendChild(programName);
+
+        departmentRow.appendChild(departmentAndProgramText);
+
+        
+        var departmentLink = document.createElement("A");
+        departmentLink.classList.add("details-link");
+        departmentLink.innerHTML = "View Programs";
+        departmentLink.style.color = "#c81912";
+        var url = "?department=" + department.departmentId;
+        departmentLink.href = "department-programs.html" + url;
+        departmentRow.onclick = function () {
+            getDepartment(department.departmentId);
+        }
+        departmentRow.appendChild(departmentLink);
+        
+        departmentRow.style.marginTop = "1rem";
+        document.getElementById("department-container").appendChild(departmentRow);
+    });
+
+    var element = document.getElementById("spinner");
+        element.parentNode.removeChild(element);
+}
+
+
+function getDepartment(departmentId) {
+    var queryString = "?department=" + departmentId;
+    window.location.href = "department-programs.html" + queryString;
+}
